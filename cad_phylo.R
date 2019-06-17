9af9f2d1fea46cf0aa2fb2abc609cf4d8ba5e516
mcadamme <mcadamme@msu.edu>
2019-06-17 12:19:52 -0400

    updated to NJ tree

--- cad_phylo.R
+++ cad_phylo.R
@@ -9,12 +9,13 @@ library(phangorn)
 library(seqinr)
 
 #reading in alignment and getting std seqinr distances
-cadherins <- read.alignment("~/Downloads/muscle-cadherin.aln", format = "clustal")
+cadherins <- read.alignment("~/Downloads/muscle-cadherin2.aln", format = "clustal")
 aa_dist <- dist.alignment(cadherins, matrix = c("identity"))
 
 cadherins_phydat <- as.phyDat(cadherins, type = "AA")
 
 mt <- modelTest(cadherins_phydat, model = c("WAG", "JTT", "LG", "Dayhoff"))
+min(mt$BIC)
 
 #modeling two best
 aa_dist2 <- dist.ml(cadherins_phydat, model = "LG")
@@ -26,11 +27,11 @@ aadist2_NJ <- NJ(aa_dist2)
 aadist3_UPGMA <- upgma(aa_dist3)
 aadist3_NJ <- NJ(aa_dist3)
 
-#looking at best trees - all have same-ish parsimony scores
+#looking at best trees - UPGMA have best parsimony scores, but evidence in literature that NJ trees are more accurate, so using those.
 parsimony(aadist2_NJ, data = cadherins_phydat)
-parsimony(aadist2_UPGMA, data = cadherins_phydat)#these two are same
+parsimony(aadist2_UPGMA, data = cadherins_phydat)
 parsimony(aadist3_NJ, data = cadherins_phydat)
-parsimony(aadist3_UPGMA, data = cadherins_phydat)#these two are same
+parsimony(aadist3_UPGMA, data = cadherins_phydat)
 
 
 #plotting two tree types for two best models protein evolution for EDA
@@ -40,14 +41,14 @@ plot(aadist2_NJ, main = "aadist2_NJ")
 plot(aadist3_UPGMA, main = "aadist3_UPGMA")
 plot(aadist3_NJ, main = "aadist3_NJ")
 
-#getting bootstrap support
-fit <- pml(aadist2_NJ, cadherins_phydat)
+#getting bootstrap support for the tree
+fit <- pml(aadist3_NJ, cadherins_phydat)
 print(fit)
-fit_LG <- optim.pml(fit, model = "LG", rearrangement = "NNI")
+fit_LG <- optim.pml(fit, model = "WAG", rearrangement = "NNI")
 logLik(fit_LG)
 bs <- bootstrap.pml(fit_LG, bs=1000, optNni=TRUE, multicore=TRUE, control = pml.control(trace=0))
 
+
 #final plot
-plotBS(midpoint(fit_LG$tree), bs, p = 10, type="p", bs.col = "red", cex = 1.2, bs.adj = c(1.1, 1.2))
-
-
+treeBS <- plotBS(midpoint(fit_LG$tree), bs, p = 10, type="p", cex = 1.1, bs.col = "red",  bs.adj = c(1.8, 0.9))
+add.scale.bar()
