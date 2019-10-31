4b012c69947de6f4c8631da345ffc5b4eee7450c
mcadamme <mcadamme@msu.edu>
2019-10-31 14:47:16 -0400

    Add files via upload

--- msms_sim_analysis.R
+++ msms_sim_analysis.R
@@ -1,10 +1,50 @@
 #Script to get distribution of FST values under neutral model using MSMS simulated genos
 #103012019
 
-setwd("/home/megan/ms_sims/test")
+#Loading packages
+x <- c("adegenet", "hierfstat")
 
-#function for weir and cockerham's theta goes here'
+lapply(x, FUN = function(X) {
+  do.call("library", list(X)) 
+})
 
-#for loop using *_output.txt
+#function to paste alleles 1 and 2 together on same line.
+f <- function(x) {
+  s <- seq(2, length(x), 2)
+  paste(x[s-1], x[s], sep="|")
+}
 
-data <- read.csv("2.txt_output.txt", header = F)
+out.file <- ""
+
+file.names <- dir("/home/megan/ms_sims/", pattern ="_output.txt")
+for(i in 1:length(file.names)){
+  
+  #df <- read.csv(file.names[i], header = F)
+  df <- read.csv("999.txt_output.txt", header = F)
+
+  # run algorithm for each column
+  df2 <- as.data.frame(lapply(df, f), stringsAsFactors=FALSE)
+  pop <- c("GA", "GA", "GA", "GA", "GA", "GAR", "GAR", "GAR", "GAR", "GAR")
+
+
+  df2_genind <- df2genind(df2, pop=pop, sep = "\\|", ploidy = 2)
+  df2_genind_sum <- summary(df2_genind)
+  df2_hier <- genind2hierfstat(df2_genind, pop = pop)
+
+  FST_mat <- pairwise.WCfst(df2_hier, diploid = T)
+  print(FST_mat[1,2])
+  out.file <- rbind(out.file, FST_mat[1,2])
+}
+
+
+write.table(out.file, "/home/megan/ms_sims/FST_msSims.out", row.names = F, col.names = F)
+
+out.file_num <- as.numeric(as.character(out.file[-1,]))
+Prob <- sum(out.file_num > 0.44)#lowest 40kb FST val in Cad86c region
+print(Prob)
+
+png(filename = "Dist_Fst_Neutral.png", units = "px", height = 600, width = 700)
+hist(out.file_num, breaks = 1000, xlim = c(-0.1, 0.2), ylim = c(0,15))
+dev.off()
+
+
--- script_to_run_msms.sh
+++ script_to_run_msms.sh
@@ -5,7 +5,7 @@
 cd /home/megan/ms_sims
 
 #simulating initial pop based on No = 600, u = 2e-9 (from Anderson et al. 2018), and r (from Martin et al. 2019).
-java -jar /home/megan/src/msms/lib/msms.jar 20 1000 -N 600 -t 0.19 -r 3.8
+#java -jar /home/megan/src/msms/lib/msms.jar 20 1000 -N 600 -t 0.19 -r 3.8
 
 #Adding switches & outputting msms file
 java -jar /home/megan/src/msms/lib/msms.jar 20 1000 -N 600 -t 0.19 -r 3.8 -I 4 5 5 5 5 \
@@ -16,10 +16,13 @@ java -jar /home/megan/src/msms/lib/msms.jar 20 1000 -N 600 -t 0.19 -r 3.8 -I 4 5
 -en 0.0154 4 1.5 \
 -ej 0.028 4 2 \
 -eN 0.029 0.1 \
--eN 0.03 167 > ms_neutralModel_sim.txt
+-eN 0.03 167 > /home/megan/ms_sims/ms_neutralModel_sim.txt
 
 #splitting file into 1000 genotype matrices
 cat /home/megan/ms_neutralModel_sim.txt | awk 'BEGIN {RS="//";FS="\n";OFS="\n"}{$1=$2=$3="";print $0 > NR ".txt"}'
+
+#for some reason added awk produced an extra file, so I am removing.
+rm ./1.txt
 
 #adding delimiter for each snp
 for file in *.txt;
@@ -27,5 +30,6 @@ do
 cat $file | gawk '{$1=$1}1' FPAT='.{1}' OFS=, > "$file"_output.txt
 done
 
-#calling R to calc FST for each sim
-R CMD BATCH "/home/megan/scripts/Tabashnik_work/msms_sim_analysis.R"
+#Next, run msms_sim_analysis.R
+
+
