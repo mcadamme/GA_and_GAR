13014a7c33b486f8017b8f153f6a2c93d6d5876d
mcadamme <mcadamme@msu.edu>
2019-12-04 13:53:32 -0500

    changed final two -eN to -en

--- script_to_run_msms.sh
+++ script_to_run_msms.sh
@@ -5,20 +5,6 @@
 cd /home/megan/ms_sims
 
 #simulating initial pop based on No = 600, u = 2e-9 (from Anderson et al. 2018), and r (from Martin et al. 2019).
-#java -jar /home/megan/src/msms/lib/msms.jar 20 1000 -N 600 -t 0.19 -r 3.8
-
-#Basic model, Adding switches & outputting msms file
-java -jar /home/megan/src/msms/lib/msms.jar 20 1000 -N 600 -t 0.19 -r 3.8 -I 4 5 5 5 5 \
--em 0.01 3 4 12.5 -em 0.01 4 3 12.5 -em 0.014 1 2 12.5 -em 0.014 2 1 12.5 \
--ej 0.0104 3 4 -ej 0.0146 1 2 \
--em 0.015 2 4 50.0 \
--en 0.0154 2 1.5 \
--en 0.0154 4 1.5 \
--em 0.0154 2 4 0 \
--ej 0.028 4 2 \
--en 0.029 2 0.1 \
--en 0.03 2 167 > /home/megan/ms_sims/ms_neutralModel_sim.txt
-
 
 #modified model based on YC comments with equal number generations (n = 72) run on 11/8/2019
 java -jar /home/megan/src/msms/lib/msms.jar 20 10000 -N 600 -t 0.19 -r 3.8 -I 4 5 5 5 5 \
