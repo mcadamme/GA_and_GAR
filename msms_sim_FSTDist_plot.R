8388299e01f5bebd954b71e2683f186fbda2dd2a
mcadamme <mcadamme@msu.edu>
2019-12-06 16:16:46 -0500

    made file paths generic

--- msms_sim_FSTDist_plot.R
+++ msms_sim_FSTDist_plot.R
@@ -1,21 +1,24 @@
 #this is my script to plot the FST distributions for my simulations
+#must change file paths for each simulation
 #11/8/2019 M. Fritz
 
-data_1 <- read.table("~/ms_sims/FST_msSims1.out", header = F)
-data_2 <- read.table("~/ms_sims/FST_msSims2.out", header = F)
+data_1 <- read.table("/PATH/TO/FILE1", header = F)
+data_2 <- read.table("/PATH/TO/FILE2", header = F)
 
 #checking to make sure I grabbed the right half of the sims for each of these
-tail(data_1, n = 1)
-head(data_2, n = 1)
+print(tail(data_1, n = 1))
+print(head(data_2, n = 1))
 
 full_data <- rbind(head(data_1, n = 5000), data_2)
-nrow(full_data)
+print(nrow(full_data))
 
-out.file_num <- as.numeric(as.character(full_data[-1,]))
+out.file_num <- as.numeric(as.character(full_data$V1))
 Prob <- sum(out.file_num > 0.44)#lowest 40kb FST val in Cad86c region
-print(Prob)
+print(Prob/10000)
 
-png(filename = "~/ms_sims/Dist_Fst_Neutral_Final.png", units = "px", height = 600, width = 700)
+png(filename = "/PATH/TO/OUTFILE/Dist_Fst_Neutral.png", units = "px", height = 600, width = 700)
 xlab.text = expression('F'['ST']*' value')
-hist(out.file_num, breaks = 100, xlim = c(-0.1, 0.4), ylim = c(0,600), main = "", ylab = "Frequency", xlab = xlab.text, cex.lab = 1.3)
+hist(out.file_num, breaks = 25, xlim = c(-0.1, 1), ylim = c(0,2000), main = "", ylab = "Frequency", xlab = xlab.text, cex.lab = 1.3)
 dev.off()
+
+
